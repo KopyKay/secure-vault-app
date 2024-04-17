d6e7a2e748eb58ff279bbb571cb49fd8277fe7bc
KopyKay <kopacz077@gmail.com>
2024-04-17 23:32:23 +0200

    Added file blob converter
    Files can be converted into a byte array using this class, facilitating storing files in binary form in the database.

---
+++ SecureVaultApp/Converter/FileBlobConverter.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+namespace SecureVaultApp.Converter
+{
+    public class FileBlobConverter
+    {
+        private byte[] blobData = null;
+
+        public byte[] FileToBlob(string filePath)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    using (BinaryReader br = new BinaryReader(fs))
+                    {
+                        this.blobData = br.ReadBytes((int)fs.Length);
+                    }
+                }
+                return this.blobData;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void BlobToFile(byte[] blobData, string filePath)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                {
+                    fs.Write(blobData, 0, blobData.Length);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
