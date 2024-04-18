2300d92836cddfee6fa422df684bd8bf91d8f97e
KopyKay <kopacz077@gmail.com>
2024-04-18 12:12:16 +0200

    Added upload file button to my vault page

---
+++ SecureVaultApp/Controls/VaultUploadFile.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace SecureVaultApp.Controls
+{
+    public sealed partial class VaultUploadFile : UserControl
+    {
+        public VaultUploadFile()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
