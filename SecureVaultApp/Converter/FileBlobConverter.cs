1d14f97d9eb879a0da7c0b62de68a07110c4ca88
KopyKay <kopacz077@gmail.com>
2024-04-19 19:04:58 +0200

    Removed try-catch block from FileBlobConverter
    Using try-catch blocks here doesn't serve any purpose.

--- SecureVaultApp/Converter/FileBlobConverter.cs
+++ SecureVaultApp/Converter/FileBlobConverter.cs
@@ -4,39 +4,25 @@ namespace SecureVaultApp.Converter
 {
     public class FileBlobConverter
     {
-        private byte[] blobData = null;
+        private byte[] _blobData = null;
 
         public byte[] FileToBlob(string filePath)
         {
-            try
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
-                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                using (BinaryReader br = new BinaryReader(fs))
                 {
-                    using (BinaryReader br = new BinaryReader(fs))
-                    {
-                        this.blobData = br.ReadBytes((int)fs.Length);
-                    }
+                    _blobData = br.ReadBytes((int)fs.Length);
                 }
-                return this.blobData;
             }
-            catch
-            {
-                throw;
-            }
+            return _blobData;
         }
 
         public void BlobToFile(byte[] blobData, string filePath)
         {
-            try
+            using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
             {
-                using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
-                {
-                    fs.Write(blobData, 0, blobData.Length);
-                }
-            }
-            catch
-            {
-                throw;
+                fs.Write(blobData, 0, blobData.Length);
             }
         }
     }
