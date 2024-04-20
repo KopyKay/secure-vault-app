7500b1c6908fb7dc5cc782f59ec58c08a68333a7
KopyKay <kopacz077@gmail.com>
2024-04-21 01:16:03 +0200

    Added "add credential" button

---
+++ SecureVaultApp/Controls/VaultAddCredential.xaml.cs
@@ -0,0 +1,17 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace SecureVaultApp.Controls
+{
+    public sealed partial class VaultAddCredential : UserControl
+    {
+        public VaultAddCredential()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
