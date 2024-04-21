014b0420643903614fb9c95ac3f96c10bc1e5334
KopyKay <kopacz077@gmail.com>
2024-04-21 17:55:28 +0200

    Moved upload button to files page
    - Removed upload file and add credential user control button

--- SecureVaultApp/View/Page/FilesPage.xaml.cs
+++ SecureVaultApp/View/Page/FilesPage.xaml.cs
@@ -3,18 +3,25 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Navigation;
 using SecureVaultApp.Controller;
+using SecureVaultApp.Converter;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using Windows.Storage.Pickers;
 
 namespace SecureVaultApp.View.Page
 {
     public sealed partial class FilesPage : Microsoft.UI.Xaml.Controls.Page
     {
         private AppController _appController;
+        private FileBlobConverter _converter;
         private List<string> _sortByOptions;
 
         public FilesPage()
         {
+            _converter = new FileBlobConverter();
+
             this.InitializeComponent();
 
             _listViewButton.IsChecked = true;
@@ -63,6 +70,32 @@ namespace SecureVaultApp.View.Page
                 default:
                     _vaultFilesCollection.ItemsPanel = listView;
                     break;
+            }
+        }
+
+        private async void UploadFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openPicker = new FileOpenPicker();
+            var window = View.Window.AuthorizationWindow._mainWindow;
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+
+            WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
+
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.FileTypeFilter.Add("*");
+
+            var file = await openPicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                try
+                {
+                    var blob = _converter.SaveFileAsBlob(file);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
             }
         }
     }
